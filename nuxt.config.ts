ec48a7510324ff69a954e420548d3639fa726ad7
workBee <contact@workbee.design>
2025-12-15 16:41:33 +0100

    Add project files and folders, composables, components, assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-12-15',
+  devtools: { enabled: true },
+  
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
+  
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
